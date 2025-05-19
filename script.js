1044ad3d9b5e74f5a35aedacf4ea9c043bd62148
Priya-Coder-09 <priyaverma2008.pv@gmail.com>
2025-05-19 12:27:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const menubtn = document.getElementById('menu-btn');
+const sidebar = document.getElementById('sidebar');
+const closebtn = document.getElementById('close-btn');
+const overlay = document.getElementById('overlay');
+
+menubtn.addEventListener('click', () => {
+    sidebar.classList.remove('hidden');
+    sidebar.classList.add('active');
+    overlay.classList.remove('hidden');
+});
+
+closebtn.addEventListener('click', closeSidebar);
+overlay.addEventListener('click', closeSidebar);
+
+function closeSidebar() {
+    sidebar.classList.remove('active');
+    sidebar.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
